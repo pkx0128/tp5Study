945b0f604bd6512399ec53bcd04dbcb03f3adaa5
pankx <764670547@qq.com>
2019-06-06 13:21:13 +0800

    默认方式设置配置项,支持二级配置

--- config/config.php
+++ config/config.php
@@ -1,4 +1,11 @@
 <?php
-    return['app_debug'=>true];
+// 默认方式设置配置项
+    return[
+        'app_debug'=>true,
+        'myinfo'=>[
+            'message'=>'默认方式设置配置项',
+            'message2'=>'支持二级配置'
+        ],
+];
 
 ?>
