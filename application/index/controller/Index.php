7f76e9c5243349bd6cd4fa3bf52a05e5166415ce
pankx <764670547@qq.com>
2019-06-08 14:57:06 +0800

    使用助手函数读取配置文件的值

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,13 +5,14 @@ class Index
 {
     public function index()
     {
-        // 加载任意位置的php文件类型的配置文件
-        \think\Config::load(APP_PATH.'../config/config.php');
-        // 加载任意位置的xml文件类型的配置文件 
-        \think\Config::parse(APP_PATH.'../config/config.xml','xml');
-         // 加载任意位置的ini文件类型的配置文件 
-         \think\Config::parse(APP_PATH.'../config/config.ini','ini');       
-        return dump(\think\Config::get());
+        // // 加载任意位置的php文件类型的配置文件
+        // \think\Config::load(APP_PATH.'../config/config.php');
+        // // // 加载任意位置的xml文件类型的配置文件 
+        // // \think\Config::parse(APP_PATH.'../config/config.xml','xml');
+        //  // 加载任意位置的ini文件类型的配置文件 
+        //  \think\Config::parse(APP_PATH.'../config/config.ini','ini');       
+        // return dump(\think\Config::get());
+        return dump(config());
 
     }
 }
