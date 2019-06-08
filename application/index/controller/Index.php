4778667cd78aae68d07f07381eb6ba5e425105a2
pankx <764670547@qq.com>
2019-06-08 14:48:55 +0800

    加载任意位置的ini文件类型的配置文件的方法

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,7 +8,9 @@ class Index
         // 加载任意位置的php文件类型的配置文件
         \think\Config::load(APP_PATH.'../config/config.php');
         // 加载任意位置的xml文件类型的配置文件 
-        \think\Config::parse(APP_PATH.'../config/config.xml');
+        \think\Config::parse(APP_PATH.'../config/config.xml','xml');
+         // 加载任意位置的ini文件类型的配置文件 
+         \think\Config::parse(APP_PATH.'../config/config.ini','ini');       
         return dump(\think\Config::get());
 
     }
