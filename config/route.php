e86d77e4537f216148c7aeb15448f8d338728bbd
pankx <764670547@qq.com>
2019-06-11 18:27:03 +0800

    用配置数组方式设置路由规则

--- config/route.php
+++ config/route.php
@@ -1,5 +1,8 @@
 <?php
 // 定义一条动态路由规则（路由中有变量传入的为动态路由）
-    think\Route::rule('test/:name','edu/test/test','GET',['ext'=>'html'],['name'=>'\w{1,10}']);
-
+    // think\Route::rule('test/:name','edu/test/test','GET',['ext'=>'html'],['name'=>'\w{1,10}']);
+// 配置数组方式设置路由规则
+    return[
+        'test/:name'=>['edu/test/test',['method'=>'GET','ext'=>'html'],['name'=>'\w{1,10}']]
+    ];
 ?>
