ddfe81ff2ab062be0d78376db57c4acd3c3d3802
pankx <764670547@qq.com>
2019-06-11 18:16:04 +0800

    设置动态路由

--- application/edu/controller/Test.php
+++ application/edu/controller/Test.php
@@ -2,9 +2,9 @@
     namespace app\edu\controller;
     class Test
     {
-        public function test()
+        public function test($name)
         {
-            return "我是Edu模块下的test方法";
+            return "我是Edu模块下的test方法，你传入的参数值为：".$name;
         }
     }
 
--- config/route.php
+++ config/route.php
@@ -1,5 +1,5 @@
 <?php
-// 定义一条路由规则
-    think\Route::rule('test','edu/test/test');
+// 定义一条动态路由规则（路由中有变量传入的为动态路由）
+    think\Route::rule('test/:name','edu/test/test','GET',['ext'=>'html'],['name'=>'\w{1,10}']);
 
 ?>
